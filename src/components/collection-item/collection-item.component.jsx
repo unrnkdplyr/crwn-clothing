8aecc58409a71c5037c31c15b215ba4d90ae1885
unrnkd.plyr <unrnkd.plyr@gmail.com>
2021-05-25 14:43:22 +0800

    updating our app with shop page, collection preview component and collection item component

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,16 @@
+import "./collection-item.styles.scss";
+
+const CollectionItem = ({ imageUrl, name, price }) => (
+  <div className="collection-item">
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
+  </div>
+);
+
+export default CollectionItem;
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,18 @@
+import CollectionItem from "../collection-item/collection-item.component";
+
+import "./collection-preview.styles.scss";
+
+const CollectionPreview = ({ items, title }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((_, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
